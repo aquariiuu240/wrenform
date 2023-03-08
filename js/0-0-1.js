7451b0f65aa7f5c14e848278d16f3ecb269c9744
aquariiuu <72821326+aquariiuu240@users.noreply.github.com>
2023-03-07 22:44:02 -0800

    Update 0-0-1.js

--- js/0-0-1.js
+++ js/0-0-1.js
@@ -1,3 +1,12 @@
-function wrender(obj){
+const wf = { default: {
+  s5: 2000,
+  s4: 1500,
+  s3: 1000,
+  s2: 800,
+  s1: 400
+}
+}
 
+function wrender(obj){
+  const wrens = document.querySelector(".w");
 }
